623add0a89c481db585f143160ab1bee2280142b
runningpark <proxent@naver.com>
2025-11-16 22:00:01 -0800

    Write Dockerfile

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,5 +1,5 @@
 // src/prisma.ts
-import { PrismaClient } from './generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient;
